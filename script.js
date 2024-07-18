e563201aeb80c92d4e32a1f28ebc650a19bcbbbe
Travisscott998 <guilhermebs220501@gmail.com>
2024-07-17 21:32:13 -0300

    upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() { var pageTitle = document.title;
+    if (pageTitle === "Travis Scott"){
+        alert("A Bio do Travis scott!!");
+    } else if (pageTitle === "Contato") {
+        alert("Seja Bem-vindo a minha 2ªPágina!");
+    } 
+    };
